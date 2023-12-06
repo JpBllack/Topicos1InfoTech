2aceb617f732d97d3a77ed57a51fd0662b365fb3
JpBllack <joaogomes@unitins.br>
2023-12-06 11:25:54 -0300

    commit padrão

--- src/main/java/org/acme/model/Telefone.java
+++ src/main/java/org/acme/model/Telefone.java
@@ -1,8 +1,6 @@
 package org.acme.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
